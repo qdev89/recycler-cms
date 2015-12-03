6c40c05a03ad1df1757cd1d15ca63607f55adbd8
Phat Tran <vanphat.tran93@gmail.com>
2015-12-03 23:54:38 +0700

    show updated and new spots

--- js/modules/spotImport/spotImportController.js
+++ js/modules/spotImport/spotImportController.js
@@ -5,17 +5,38 @@
     .module('recycler')
     .controller('sportImportController', ['$scope', 'sportImportFactory',
       function ($scope, sportImportFactory) {
+        $scope.newSpots = [];
+        $scope.updatedSpots = [];
 
         sportImportFactory.getSpotsFromMaptive()
-          .then(function(result) {
+          .then(function (result) {
             $scope.maptiveSpots = result;
 
             sportImportFactory.getSpotsFromDb()
               .then(function (result) {
                 $scope.telerikSpots = result;
 
-                // Compare differences
+                _.forEach($scope.telerikSpots.result, function (item, index) {
+                  item.Latitude = parseFloat(item.Latitude, 10);
+                  item.Longitude = parseFloat(item.Longitude, 10);
+                });
 
+                var sortedTelerikSpots = _.sortBy($scope.telerikSpots.result, 'Latitude');
+                var sortedMaptiveSpots = _.sortBy($scope.maptiveSpots.markers, 'lt');
+
+                _.forEach(sortedTelerikSpots, (function (telerikSpot) {
+                  _.forEach(sortedMaptiveSpots, function (maptiveSpot, index) {
+                    if (telerikSpot.Latitude == maptiveSpot.lt) {
+                      if (telerikSpot.Longitude == maptiveSpot.lg) {
+                        sortedMaptiveSpots.splice(index, 1);
+                        $scope.updatedSpots.push(maptiveSpot);
+                      }
+                    } else {
+                      $scope.newSpots.push(maptiveSpot);
+                      $scope.newSpots = _.uniq($scope.newSpots);
+                    }
+                  })
+                }))
               });
           });
       }])
